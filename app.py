c38ae6e709f9d9351085f840bfe045cc29db70ef
Allan Chen <allanmc@umich.edu>
2018-02-04 14:20:13 -0500

    initial commit, setup files/folders using Flask and SQLalchemy

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, render_template, url_for, flash, redirect, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, RadioField, IntegerField, ValidationError
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+import requests
+import json
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hardtoguessstring'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = ""
+## Provided:
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class WeatherEntryForm(FlaskForm):
+    zip= StringField("Enter Zip code:", validators=[Required()])
+    submit = SubmitField('Submit')
+
+    def validate_zip(self, field):
+        if len(field.data) != 5:  # REPLACE THIS LINE WITH SOMETHING ELSE
+            raise ValidationError("Your zipcode did not contain 5 integers")
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/zipcode')
+def weather_form():
+    weather = WeatherEntryForm()
+    return render_template("weather_form.html", form=weather)
+
+@app.route('/weather_result', methods=['GET', 'POST'])
+def show_results():
+    form = WeatherEntryForm()
+    if request.method == "POST" and form.validate_on_submit():
+        zip = form.zip.data
+        r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&appid=acb8954b723c21c5d82de826a487e67a')
+        r = r.json()
+        city = r["name"]
+        description = r['weather'][0]['description']
+
+        temp = 9/5 * (r['main']['temp'] - 273) + 32
+
+        return render_template('weather_results.html', city = city, description= description, temp = temp)
+    flash(form.errors)
+    return redirect(url_for('weather_form'))
+
+if __name__ == '__main__':
+    app.run()
+
+
+
